5a227ef7b219ea885219075284e1e18a52904fcc
Jisup <xrl0603@naver.com>
2021-02-16 19:34:54 +0900

    2-16_solving_bitmasking

---
+++ SWExpertAcademy/Java/_5215_햄버거_다이어트.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+class Data {
+	int sf;
+	int cal;
+	Data(int sf, int cal) {
+		this.sf=sf;
+		this.cal=cal;
+	}
+}
+public class _5215_햄버거_다이어트 {
+	static final int MAX = 21;
+	static int N, max_cal;
+	static Data food[] = new Data[MAX];
+	
+	static void init() {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for(int t=1;t<=tc;t++) {
+			N = sc.nextInt();
+			max_cal = sc.nextInt();
+			
+			for(int i=0;i<N;i++)
+				food[i] = new Data(sc.nextInt(), sc.nextInt());
+			
+			System.out.print("#"+t+" ");
+			solve(1<<N);
+		}
+	}
+	static void solve(int idx) {
+		int result=0;
+		for(int index=0;index<idx;index++) {
+			int sum_sf=0;
+			int sum_cal=0;
+			for(int i=0;i<N;i++) {
+				int value = 1<<i;
+//				System.out.println(value+" "+index+" "+(index&value));
+				 if ((index & value) != 0) {
+					sum_sf += food[i].sf;
+					sum_cal += food[i].cal;
+				 }
+			}
+			if (sum_cal > max_cal) continue;
+			result = result > sum_sf ? result:sum_sf;
+		}
+		System.out.println(result);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		init();
+	}
+}
